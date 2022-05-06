ebfcdfb4227327a5a3bc69b4884f173e726abb8e
Aditya Singh <adisingh0309@gmail.com>
2022-05-06 13:48:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="main">
+      <div class="container">
+        <form>
+          <h2>Send us a Message!</h2>
+
+          <label for="name" className="label" ></label>
+          <input id ="name" input type="text" placeholder='Name'/><br></br><br></br>
+
+          <label for="email" className="label" ></label>
+          <input id ="email" input type="email" placeholder='Email' /><br></br><br></br>
+
+          <label for="message" className="Message" ></label>
+
+          <textarea rows="6" cols="100" name="message" placeholder='Message' ></textarea><br></br>
+          <button>Send</button>
+          </form>
+          </div>
     </div>
   );
 }
